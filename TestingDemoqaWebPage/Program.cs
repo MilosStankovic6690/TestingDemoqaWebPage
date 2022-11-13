3abeb221d3f898190a1371899332429580ba9282
MilosStankovic6690 <Milosstankovic6690@gmail.com>
2022-11-13 12:17:09 +0100

    Add project files.

---
+++ TestingDemoqaWebPage/Program.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingDemoqaWebPage
+{
+
+
+    public class Program
+    {
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+          
+        }
+
+        [SetUp]
+        public void Podesavanja()
+        {
+            driver.Navigate().GoToUrl("https://demoqa.com/automation-practice-form");
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        
+        public void Test()
+        {
+            IWebElement name = driver.FindElement(By.Id("firstName"));
+            name.SendKeys("Milos");
+
+            IWebElement LastName = driver.FindElement(By.Id("lastName"));
+            LastName.SendKeys("Stankovic");
+
+            IWebElement email = driver.FindElement(By.Id("userEmail"));
+            email.SendKeys("milosstankovic6690@gmail.com");
+
+            IWebElement currentAdress = driver.FindElement(By.Id("currentAddress"));
+            currentAdress.SendKeys("Spasenoviceva");
+
+            IWebElement button = driver.FindElement(By.Id("submit"));
+            button.Submit();
+        }
+
+        [TearDown]
+
+        public void ZatvaranjeStranice()
+        { 
+            driver.Close();
+        }
+    }
+}
